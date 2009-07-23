991871297375f240e1f4ea453b8d74b2748cf2c5
ausiv4 <ausiv4@eb105b4a-77de-11de-a249-6bf219df57d5>
2009-07-23 23:49:28 +0000

    Test commit

--- django/srpproject/settings.py
+++ django/srpproject/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
 DATABASE_NAME = 'srp'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'USER'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'PASSWORD'         # Not used with sqlite3.
+DATABASE_PASSWORD = 'SECRET_PASSWORD'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
