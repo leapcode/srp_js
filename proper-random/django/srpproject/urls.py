40fd75696b548d18057a289e849be98dd2af12dd
ausiv4 <ausiv4@eb105b4a-77de-11de-a249-6bf219df57d5>
2009-07-24 13:57:46 +0000

    Not what I meant to do...

---
+++ proper-random/django/srpproject/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+from srpproject.srp import views
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^srpproject/', include('srpproject.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/(.*)', admin.site.root),
+    (r'^srp/register/salt/$', views.register_salt),
+    (r'^srp/register/user/$', views.register_user),
+    (r'^srp/handshake/$', views.handshake),
+    (r'^srp/authenticate/$', views.verify),
+    (r'^srp/login/$', views.login_page),
+    (r'^srp/register/$', views.register_page),
+)
