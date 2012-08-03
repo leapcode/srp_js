94d1938e2e5d0ee5e8e7b9a8ed44a067677e0133
Azul <azul@leap.se>
2012-08-03 20:37:11 +0200

    moved all xhr related stuff to a seperate class
    We can replace this if we want to use jquery ajax or similar. Also this has all the urls so it's super easy to overwrite

--- spec/signup.js
+++ spec/signup.js
@@ -24,11 +24,11 @@ describe("Signup", function() {
 
   it("receives the salt from /register/salt", function(){
     var callback = sinon.spy();
-    this.srp.register_send_verifier = callback;
+    this.srp.remote.register_send_verifier = callback;
     this.srp.register();
     this.expectRequest('register/salt/', "I=user")
     this.respondXML("<salt>5d3055e0acd3ddcfc15</salt>");
-    expect(callback).toHaveBeenCalledWith("adcd57b4a4a05c2e205b0b7b30014d9ff635d8d8db2f502f08e9b9c132800c44");
+    expect(callback).toHaveBeenCalledWith("adcd57b4a4a05c2e205b0b7b30014d9ff635d8d8db2f502f08e9b9c132800c44", this.srp.registered_user);
   });
 
   it("identifies after successful registration (INTEGRATION)", function(){
