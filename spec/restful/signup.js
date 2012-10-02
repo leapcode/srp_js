7791caac4f3dc8f9b2fcca394ad9ce0cd12b6996
Azul <azul@riseup.net>
2012-10-02 14:29:20 +0200

    fixed restful signup test

--- spec/restful/signup.js
+++ spec/restful/signup.js
@@ -16,7 +16,7 @@ describe("Signup", function() {
   it("identifies after successful registration (INTEGRATION)", function(){
     var callback = sinon.spy();
     this.srp.identify = callback;
-    this.srp.getSalt = function() {return "5d3055e0acd3ddcfc15"};
+    this.srp.session.getSalt = function() {return "5d3055e0acd3ddcfc15"};
     this.srp.register();
     this.expectRequest('users', "user[login]=user&user[password_salt]=5d3055e0acd3ddcfc15&user[password_verifier]=adcd57b4a4a05c2e205b0b7b30014d9ff635d8d8db2f502f08e9b9c132800c44")
     this.respondJSON({password_salt: "5d3055e0acd3ddcfc15", login: "user", ok: "true"});
